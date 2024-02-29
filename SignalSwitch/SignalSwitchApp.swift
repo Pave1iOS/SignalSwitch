d1cbc18d55bf5ea0e140b4870641eeac995218e3
Pave1iOS <PGribachev94@yandex.ru>
2024-02-29 12:07:50 +0300

    Initial Commit

---
+++ SignalSwitch/SignalSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalSwitchApp.swift
+//  SignalSwitch
+//
+//  Created by Pavel Gribachev on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SignalSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
